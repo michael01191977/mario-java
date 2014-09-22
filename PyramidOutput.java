810b483b96041f48fe129b23e469d191ca516362
michael01191977 <michael01191977@hotmail.com>
2014-09-22 15:32:23 -0500

    Interface for Console and Text File class

---
+++ PyramidOutput.java
@@ -0,0 +1,8 @@
+package marioOuput;
+
+public interface PyramidOutput {
+
+	public void setPyramidRow(String pyramidRow);
+	public void prepareOutputDestination();
+	public void printPyramidRow();
+}
