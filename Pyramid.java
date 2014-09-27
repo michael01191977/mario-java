a223d832327597d50993dd476dc21f721475bf6f
michael01191977 <michael01191977@hotmail.com>
2014-09-27 15:34:38 -0500

    Factory Design Pattern and getString() Change

--- Pyramid.java
+++ Pyramid.java
@@ -1,8 +1,40 @@
-public class Pyramid
-{
-	public static void main(String[] argv)
+import marioOutput.PyramidOutput;
+
+public class Pyramid {
+	
+	static int MAXIMUM_NUMBER_OF_PYRAMID_ROWS = 23;
+	static int MINIMUM_NUMBER_OF_BLOCKS = 2;
+	static char SPACE = ' ';
+	static char BRICK = '#';
+
+	private int pyramidHeight;
+	private PyramidOutput pyramidOutput;
+	
+	public Pyramid(int pyramidHeight, PyramidOutput pyramidOutput)
 	{
-		Mario mario1 = new Mario();
-		mario1.assemblePyramid();
-	}	
+		this.pyramidHeight = pyramidHeight;
+		this.pyramidOutput = pyramidOutput;
+	};
+	
+    public void printAllPyramidRows()
+    {  
+       for(int numberOfSpaces = this.pyramidHeight + 1 - MINIMUM_NUMBER_OF_BLOCKS, numberOfBricks = MINIMUM_NUMBER_OF_BLOCKS; numberOfBricks <= this.pyramidHeight + 1;  numberOfSpaces--, numberOfBricks++)
+       {
+           StringBuilder pyramidRow = new StringBuilder();
+           pyramidRow.append(addBlockToPyramidRow(numberOfSpaces, SPACE));
+           pyramidRow.append(addBlockToPyramidRow(numberOfBricks, BRICK));
+           this.pyramidOutput.printPyramidRow(pyramidRow.toString());
+       }
+    }
+    
+    private String addBlockToPyramidRow(int countOfBlocks, char block)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < countOfBlocks; i++)
+        {
+            sb.append(block);
+        }
+        return sb.toString();
+    }	
 }
+
