a443dcb4499331f183e86ade476efe0c240df9e1
michael01191977 <michael01191977@hotmail.com>
2014-10-03 08:53:34 -0500

    dependecy injection using spring and maven

--- PyramidTest.java
+++ PyramidTest.java
@@ -1,8 +1,12 @@
-public class PyramidTest
-{
-	public static void main(String[] argv)
-	{
-		Mario mario1 = new Mario();
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import main.java.Mario;
+
+public class PyramidTest {
+	public static void main(String[] argv) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+		Mario mario1 = (Mario) context.getBean("mario1");
 		mario1.assemblePyramid();
-	}	
+	}
 }
